4e0cc29cb7532bcd7329d4139b07fb6caedd34f6
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2024-01-17 16:17:42 +0100

    fix

--- src/main.ts
+++ src/main.ts
@@ -1,5 +1,19 @@
 import { createApp } from 'vue'
 import './style.css'
 import App from './App.vue'
+import { axios } from "./lib/axios"
 
-createApp(App).mount('#app')
+async function loginAndMakeApp() {
+  const data = {
+    "username":"@angolo180",
+    "password":"Angolo.180"
+}
+
+  console.log(await axios.post<string>('/token', data))
+
+
+  createApp(App).mount('#app')
+
+}
+
+loginAndMakeApp()
