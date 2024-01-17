99cb4cd16aebce6f5ae68e0fd895368d431d8147
bugboy-20 <diegoroberto.ammirabile@gmail.com>
2024-01-17 16:32:12 +0100

    Inizio

---
+++ src/components/NewPostTxt.vue
@@ -0,0 +1,10 @@
+<script setup lang="ts">
+
+</script>
+<template>
+  <div>
+    <textarea name="f" id="" cols="30" rows="10" class="" placeholder="Nuovo post..."></textarea>
+    <hr>
+    <button>invia</button>
+  </div>
+</template>
